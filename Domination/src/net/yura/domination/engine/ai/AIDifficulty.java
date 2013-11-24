89ce9c442dc18662600f32bb13a5b50eca204dd0
cosmosferah <cosmosferah316@gmail.com>
2013-11-24 16:35:05 -0500

    Patchset1/4
    Created 2 new attributes called "type" of type int and "command" of
    type String. Used Pull Up Method on the 2 methods: "getType()" and
    "getCommand()" from the 3 difficulty subclasses (AIEasy, AIAverage,
    AIHard) to the new superclass AIDifficulty.

--- Domination/src/net/yura/domination/engine/ai/AIDifficulty.java
+++ Domination/src/net/yura/domination/engine/ai/AIDifficulty.java
@@ -2,4 +2,15 @@ package net.yura.domination.engine.ai;
 
 public abstract class AIDifficulty extends AbstractAI {
  	
+	protected int type;
+	protected String command;
+	
+	public int getType(){
+		return type;
+	}
+	
+	public String getCommand(){
+		return command;
+	}
+	
 }
