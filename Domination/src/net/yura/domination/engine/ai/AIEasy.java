4576069b854c8d5bd8fcad58b10c26eec2cf72eb
cosmosferah <cosmosferah316@gmail.com>
2013-11-24 16:56:26 -0500

    Patchset2/4
    The AIEasy class now extends the newly created AIDifficulty superclass.
    The traces of "getType()" and "getCommand()" methods were also removed
    from this class since they were pulled up to AIDifficulty on the
    previous patchset. As a result, the attributes "type" and "command" are
    directly assigned of the appropriate difficulty for this class.

--- Domination/src/net/yura/domination/engine/ai/AIEasy.java
+++ Domination/src/net/yura/domination/engine/ai/AIEasy.java
@@ -7,14 +7,9 @@ import net.yura.domination.engine.ai.logic.AIDomination;
 /**
  * @author Steven Hawkins
  */
-public class AIEasy extends AbstractAI {
-    
-    public int getType() {
-        return AIDomination.PLAYER_AI_EASY;
-    }
-
-    public String getCommand() {
-        return "easy";
-    }
-    
+public class AIEasy extends AIDifficulty {
+	{  
+		type = AIDomination.PLAYER_AI_EASY;
+		command = "easy";
+	}
 }
