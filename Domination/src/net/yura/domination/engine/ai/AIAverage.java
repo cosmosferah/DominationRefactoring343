fc26634dfa7def5dd0257654b7e29547327afbc5
cosmosferah <cosmosferah316@gmail.com>
2013-11-24 17:03:40 -0500

    Patchset3/4
    The AIAverage class now correctly extends the newly created
    AIDifficulty superclass. The traces of "getType()" and "getCommand()"
    methods were also removed
    from this class since they were pulled up to AIDifficulty on the
    previous patchset. As a result, the attributes "type" and "command" are
    directly assigned of the appropriate difficulty for this class.

--- Domination/src/net/yura/domination/engine/ai/AIAverage.java
+++ Domination/src/net/yura/domination/engine/ai/AIAverage.java
@@ -7,14 +7,9 @@ import net.yura.domination.engine.ai.logic.AIDomination;
 /**
  * @author Steven Hawkins
  */
-public class AIAverage extends AbstractAI {
-
-    public int getType() {
-        return AIDomination.PLAYER_AI_AVERAGE;
-    }
-
-    public String getCommand() {
-        return "average";
-    }
-
+public class AIAverage extends AIDifficulty {
+	{
+		type = AIDomination.PLAYER_AI_AVERAGE;
+        command = "average";
+	}
 }
